6c0500fd3f53eae2f89e1201fa235dd9e010e609
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-01-27 23:17:42 +0530

    fix css

--- frontend/src/components/ShowProjects.jsx
+++ frontend/src/components/ShowProjects.jsx
@@ -1,26 +1,31 @@
-import { Code2, TrendingUp } from "lucide-react"
+import { Code2, TrendingUp } from "lucide-react";
 
-const ShowProjects = ({projects}) => {
+const ShowProjects = ({ projects }) => {
   const handleLiveDemo = (url) => {
-    if (url) window.open(url, '_blank');
+    if (url) window.open(url, "_blank");
   };
 
   const handleSourceCode = (url) => {
-    if (url) window.open(url, '_blank');
+    if (url) window.open(url, "_blank");
   };
-
 
   return (
     <>
-    {projects.map(project => (
-      <div key={project.id} className="[28rem] mx-2 md:mx-0 lg:mb-0 px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white">
-        <div className="h-[15rem] bg-cover bg-center rounded-lg" style={{ backgroundImage: `url(${project.imageUrl})` }}></div>
+      {projects.map((project) => (
+        <div
+          key={project.id}
+          className="md:h-[28rem] mx-2 md:mx-0 lg:mb-0 px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white"
+        >
+          <div
+            className="h-[10rem] md:h-[15rem] bg-cover bg-center rounded-lg"
+            style={{ backgroundImage: `url(${project.imageUrl})` }}
+          ></div>
 
-        <div className="flex items-center justify-between mt-1">
-          <h4 className="font-bold text-2xl">{project.title}</h4>
-          <div className="flex gap-3">
-            {project.githubCode && (
-                <span 
+          <div className="flex items-center justify-between ">
+            <h4 className="font-bold text-2xl">{project.title}</h4>
+            <div className="flex gap-3">
+              {project.githubCode && (
+                <span
                   onClick={() => handleSourceCode(project.githubCode)}
                   className="transition duration-300 text-black ease-in-out bg-[#dad3d3] hover:bg-[#b4a0a0] rounded-md px-3 py-2 cursor-pointer"
                 >
@@ -28,31 +33,40 @@ const ShowProjects = ({projects}) => {
                 </span>
               )}
               {project.liveDemo && (
-                <span 
+                <span
                   onClick={() => handleLiveDemo(project.liveDemo)}
                   className="transition duration-300 text-black ease-in-out bg-[#dad3d3] hover:bg-[#b4a0a0] rounded-md px-3 py-2 cursor-pointer"
                 >
                   <TrendingUp size={18} />
                 </span>
               )}
+            </div>
+          </div>
+
+          <div className="text-gray-700 text-[1rem]">{project.description}</div>
+
+          <div className="flex gap-3">
+            {project.techStack.map((tech, index) => (
+              <span
+                key={index}
+                className=" hidden md:block transition duration-300 text-black ease-in-out bg-[#dad3d3] hover:bg-[#b4a0a0] text-xs lg:text-sm rounded-sm lg:rounded-md px-1 py-1 lg:px-3 lg:py-2"
+              >
+                {tech}
+              </span>
+            ))}
+            {project.techStackMobile.map((tech, index) => (
+              <span
+                key={index}
+                className=" block md:hidden transition duration-300 text-black ease-in-out bg-[#dad3d3] hover:bg-[#b4a0a0] text-sm lg:text-sm rounded-sm lg:rounded-md px-1 py-1 lg:px-3 lg:py-2"
+              >
+                {tech}
+              </span>
+            ))}
           </div>
         </div>
+      ))}
+    </>
+  );
+};
 
-        <div className="text-gray-700 text-[1rem]">{project.description}</div>
-
-        <div className="flex gap-3">
-          {project.techStack.map((tech, index) => (
-            <span key={index} className="transition duration-300 text-black ease-in-out bg-[#dad3d3] hover:bg-[#b4a0a0] text-xs lg:text-sm rounded-sm lg:rounded-md px-1 py-1 lg:px-3 lg:py-2">
-              {tech}
-            </span>
-          ))}
-        </div>
-      </div>
-    ))}
-  </>
-   
-
-  )
-}
-
-export default ShowProjects
+export default ShowProjects;
