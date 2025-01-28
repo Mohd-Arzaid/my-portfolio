4d52808086c53dcfb24affd88b36f16275259db3
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-01-29 00:39:34 +0530

    Send Mail fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import { ArrowRight } from "lucide-react"
-
+import { ArrowRight } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className=" lg:m-auto p-8">
-            <div className="flex md:w-64 font-medium tracking-medium flex-col gap-3 border-2 border-neutral-500  dark:border-white/20  p-4 rounded-lg">
-                <h1 className="opacity-70 text-base">Thanks for visiting my site!</h1>
-                <h1 className=" text-xl">Glad to have you here.</h1>
-                <a href="arzaid010103@gmail.com?body=Hello" target="_blank" className="flex cursor-pointer text-base items-center gap-2 duration-200 group">
-                    Send me an email <ArrowRight className=" duration-200 group-hover:translate-x-3" />
-                </a>
-            </div>
+    <footer className="lg:m-auto p-8">
+      <div className="flex md:w-64 font-medium tracking-medium flex-col gap-3 border-2 border-neutral-500 dark:border-white/20 p-4 rounded-lg">
+        <h1 className="opacity-70 text-base">Thanks for visiting my site!</h1>
+        <h1 className="text-xl">Glad to have you here.</h1>
+        <a
+          href="mailto:arzaid010103@gmail.com?body=Hello"
+          target="_blank"
+          className="flex cursor-pointer text-base items-center gap-2 duration-200 group"
+        >
+          Send me an email <ArrowRight className="duration-200 group-hover:translate-x-3" />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-            
-        </footer>
-   
-  )
-}
-
-export default Footer
+export default Footer;
